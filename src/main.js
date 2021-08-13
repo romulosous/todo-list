3160fadace16395f79546aa9350ead2d645307ce
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-08-13 16:35:34 -0300

    deletar todos

--- src/main.js
+++ src/main.js
@@ -97,6 +97,14 @@ const app = createApp({
 
       const index = this.todos.findIndex(({ id }) => id === data.id)
       this.todos[index] = data
+    },
+    async deleteTodo (id) {
+      console.log(id)
+      await apiTodos.destroy({ id })
+
+      const index = this.todos.findIndex(todo => todo.id === id)
+
+      this.todos.splice(index, 1)
     }
   }
 })
